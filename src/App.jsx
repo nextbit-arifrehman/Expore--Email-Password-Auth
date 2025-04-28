bcdc26b0c78c9aab30433fa87387321e9deb78a7
nextbit-arifrehman <asustufk513ea@gmail.com>
2025-04-29 01:24:32 +0600

    Base_SetUP

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-2xl'>Explore Email Password Authentication</h1>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3>Email Password Auth</h3>
+        </div>
+    );
+};
+
+export default Home;
